09b047687787d3c087c93615aa1a2234beabf07e
Hynek Noll <nollhyne@fel.cvut.cz>
2019-04-26 19:14:52 +0200

    Junit 5 test skeleton

---
+++ src/test/java/com/dataartisans/flinktraining/examples/datastream_java/process/CarEventSortTest.java
@@ -0,0 +1,15 @@
+package com.dataartisans.flinktraining.examples.datastream_java.process;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarEventSortTest {
+    static final String carInOrderPath = "./carInOrder.csv";
+    static final String carOutOfOrderPath = "./carOutOfOrder.csv";
+
+    @Test
+    void main() {
+        
+    }
+}
