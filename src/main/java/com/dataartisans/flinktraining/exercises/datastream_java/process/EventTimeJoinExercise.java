494051c9cd2b38d4ac5686308ecce8d0af0d2053
Hynek Noll <nollhyne@fel.cvut.cz>
2019-06-07 20:35:56 +0200

    Event Time Join - Exercise 2 (low-latency variant)

--- src/main/java/com/dataartisans/flinktraining/exercises/datastream_java/process/EventTimeJoinExercise.java
+++ src/main/java/com/dataartisans/flinktraining/exercises/datastream_java/process/EventTimeJoinExercise.java
@@ -22,6 +22,7 @@ import com.dataartisans.flinktraining.exercises.datastream_java.datatypes.Trade;
 import com.dataartisans.flinktraining.exercises.datastream_java.sources.FinSources;
 import org.apache.flink.api.common.state.MapState;
 import org.apache.flink.api.common.state.MapStateDescriptor;
+import org.apache.flink.api.common.state.ValueState;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.streaming.api.TimeCharacteristic;
@@ -81,7 +82,8 @@ public class EventTimeJoinExercise {
 		SingleOutputStreamOperator<EnrichedTrade> joinedStream = tradeStream    // an extension of DataStream class
 				.keyBy("customerId")
 				.connect(customerStream.keyBy("customerId"))
-				.process(new EventTimeJoinFunction());
+//		        .process(new EventTimeJoinFunction());
+				.process(new EventTimeLowLatencyJoinFunction());
 		
 		joinedStream.print();
 
@@ -205,4 +207,142 @@ public class EventTimeJoinExercise {
 			return theOneWeAreLookingFor;
 		}
 	}
+
+    public static class EventTimeLowLatencyJoinFunction extends CoProcessFunction<Trade, Customer, EnrichedTrade> {
+        // Store pending Trades for a customerId, keyed by timestamp
+        private MapState<Long, Trade> tradeMap = null;
+
+        // Store Customer updates for a customerId, keyed by timestamp
+        private MapState<Long, Customer> customerMap = null;
+        
+        // Store the realized immediate joins, keyed by the Trade being enriched (value is the given Customer update)
+        private MapState<Trade, Customer> joinsMap = null;
+
+        final OutputTag<EnrichedTrade> outputTag = new OutputTag<EnrichedTrade>("side-output") {}; // for late trades
+        private int LATE_TRADES_THRESHOLD = 10000; // maximum time (in ms) to wait for potential late trades
+
+        @Override
+        public void open(Configuration config) {
+            MapStateDescriptor<Long, Trade> tDescriptor = new MapStateDescriptor<>(
+                    "tradeBuffer",
+                    TypeInformation.of(Long.class),
+                    TypeInformation.of(Trade.class)
+            );
+            tradeMap = getRuntimeContext().getMapState(tDescriptor);
+
+            MapStateDescriptor<Long, Customer> cDescriptor = new MapStateDescriptor<>(
+                    "customerBuffer",
+                    TypeInformation.of(Long.class),
+                    TypeInformation.of(Customer.class)
+            );
+            customerMap = getRuntimeContext().getMapState(cDescriptor);
+            
+            MapStateDescriptor<Trade, Customer> jDescriptor = new MapStateDescriptor<Trade, Customer>(
+                    "joinsBuffer",
+                    TypeInformation.of(Trade.class),
+                    TypeInformation.of(Customer.class)
+            );
+            joinsMap = getRuntimeContext().getMapState(jDescriptor);
+        }
+
+        @Override
+        public void processElement1(Trade trade,
+                                    Context context,
+                                    Collector<EnrichedTrade> out)
+                throws Exception {
+
+            System.out.println("Received " + trade.toString());
+            TimerService timerService = context.timerService();
+
+            if (context.timestamp() > timerService.currentWatermark()) {
+                // Do the join now, and if any newer Customer updates arrive, also join later.
+                // We can use the current trade's watermark as no Customer updates older than the newest one are 
+                // expected to be useful (the onTimer method for any trades requiring those updates has already fired 
+                // on the arrival of a newer Customer update)
+                Customer newestCustomer = getCustomerRecordToJoin(trade.timestamp);
+                if (newestCustomer != null) {
+                    out.collect(new EnrichedTrade(trade, newestCustomer));
+                    joinsMap.put(trade, newestCustomer);
+                }
+                tradeMap.put(trade.timestamp, trade);
+                timerService.registerEventTimeTimer(trade.timestamp);
+            } else { // Late Trades land here.
+                // we can join them right away
+                context.output(outputTag, new EnrichedTrade(trade, getCustomerRecordToJoin(trade.timestamp)));
+            }
+        }
+
+        @Override
+        public void processElement2(Customer customer,
+                                    Context context,
+                                    Collector<EnrichedTrade> collector)
+                throws Exception {
+
+            System.out.println("Received " + customer.toString());
+            customerMap.put(customer.timestamp, customer);
+
+            /* Calling this solely for its side effect of freeing older Customer records.
+             * Otherwise Customers with frequent updates and no Trades would leak state.
+             */
+            getCustomerRecordToJoin(context.timerService().currentWatermark());
+        }
+
+        @Override
+        public void onTimer(long t,
+                            OnTimerContext context,
+                            Collector<EnrichedTrade> out)
+                throws Exception {
+
+            Trade trade = tradeMap.get(t);
+            if (trade != null) {
+                tradeMap.remove(t);
+                Customer newestCustomerUpdate = getCustomerRecordToJoin(trade.timestamp);
+                if (newestCustomerUpdate != joinsMap.get(trade)) {  // check if we haven't made the same join at arrival
+                    EnrichedTrade joined = new EnrichedTrade(trade, newestCustomerUpdate);
+                    out.collect(joined);
+                }
+                joinsMap.remove(trade);
+            }
+        }
+
+        /**
+         * Returns the newest Customer that isn't newer than the Trade we are enriching.
+         * As a side effect, removes earlier Customer records that are no longer needed.
+         */
+        private Customer getCustomerRecordToJoin(Long timestamp) throws Exception {
+            Iterator<Map.Entry<Long, Customer>> customerEntries = customerMap.iterator();
+            Customer theOneWeAreLookingFor = null;
+            List<Long> toRemove = new ArrayList<>();
+
+            while(customerEntries.hasNext()) {
+                Customer c = customerEntries.next().getValue();
+                // c should not be newer than the Trade being enriched
+                if (c.timestamp <= timestamp) {
+                    /*
+                     * By the time Trades are being joined, they are being processed in order (by Timestamp).
+                     * This means that any Customer record too old to join with this Trade is too old
+                     * to be worth keeping any longer.
+                     */
+                    if (theOneWeAreLookingFor != null) {
+                        if (c.timestamp > theOneWeAreLookingFor.timestamp) {
+                            // added late trades support
+                            if (c.timestamp > theOneWeAreLookingFor.timestamp + LATE_TRADES_THRESHOLD) {
+                                toRemove.add(theOneWeAreLookingFor.timestamp);
+                            }
+                            theOneWeAreLookingFor = c;
+                        }
+                    } else {
+                        theOneWeAreLookingFor = c;
+                    }
+                }
+            }
+
+            for (Long t : toRemove) {
+                System.out.println("Removing customer @ " + t);
+                customerMap.remove(t);
+            }
+
+            return theOneWeAreLookingFor;
+        }
+    }
 }
